6c7ad21d13cfe426a231b0e8a827bbf221842f7a
Aaron Madlon-Kay <aaron@madlon-kay.com>
2015-08-01 23:29:08 +0900

    Initial commit

---
+++ init.el
@@ -0,0 +1,66 @@
+;; Increase default font size.
+(set-face-attribute 'default nil :height 180)
+
+;; Set decent default fonts for Japanese and Chinese,
+;; but *only* if in a graphical context.
+;; Set Japanese second so that Japanese glyphs override Chinese
+;; when both charsets cover the same codepoints.
+(if (fboundp 'set-fontset-font)
+    (progn
+      (set-fontset-font
+       nil
+       'chinese-gbk
+       (font-spec :family "Hiragino Sans GB W3"))
+      (set-fontset-font
+       nil
+       'japanese-jisx0213.2004-1
+       (font-spec :family "Hiragino Kaku Gothic ProN"))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ns-pop-up-frames nil)
+ '(tab-width 4)
+ '(ediff-split-window-function 'split-window-horizontally)
+ '(ediff-window-setup-function 'ediff-setup-windows-plain))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Use aspell as installed by MacPorts.
+(setq ispell-program-name "/opt/local/bin/aspell")
+(add-hook 'text-mode-hook 'flyspell-mode)
+(add-hook 'prog-mode-hook 'flyspell-prog-mode)
+
+(setq-default indent-tabs-mode nil)
+
+(show-paren-mode)
+(column-number-mode)
+
+;; Only on GUI
+(if window-system
+    (progn
+      ;; Restore session
+      (desktop-save-mode t)
+      ;; Disable C-z (suspend-frame) in GUI because it's pointless
+      ;; and I keep hitting it.
+      (global-unset-key "\C-z")
+      ;; Run server so `emacsclient` will edit in GUI editor.
+      ;; May have to prepend /Applications/Emacs.app/Contents/MacOS/bin
+      ;; to PATH on OS X.
+      (server-start)
+      ;; ediff customizations
+      ;; https://www.ogre.com/node/446
+      (add-hook 'ediff-before-setup-hook
+                'new-frame)
+      (add-hook 'ediff-quit-hook
+                'delete-frame)
+      (add-hook 'ediff-startup-hook
+                '(lambda ()
+                   (set-frame-size (selected-frame) 175 55)
+                   (raise-frame (selected-frame))))))
