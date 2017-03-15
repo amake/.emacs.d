4adde159887f47321b3293484bf0d0496bb5b1d8
Aaron Madlon-Kay <aaron@madlon-kay.com>
2017-03-15 09:50:31 +0900

    Add org-mode package archive

--- init.el
+++ init.el
@@ -104,6 +104,8 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("org" . "http://orgmode.org/elpa/") t)
 
 ;; Improve text printing speed in Python shell
 (setq python-shell-enable-font-lock nil)
