3c8970a9b8509184ff7d30c6086f4109d5ac2cba
Aaron Madlon-Kay <aaron@madlon-kay.com>
2017-04-05 11:02:49 +0900

    Move customizations to external file

---
+++ custom-file.el
@@ -0,0 +1,37 @@
+;;; package --- Summary
+
+;;; Commentary:
+
+;;; Code:
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ediff-split-window-function (quote split-window-horizontally))
+ '(ediff-window-setup-function (quote ediff-setup-windows-plain))
+ '(fill-column 80)
+ '(global-linum-mode t)
+ '(js-indent-level 2)
+ '(mac-command-modifier (quote super))
+ '(mac-option-modifier (quote meta))
+ '(ns-pop-up-frames nil)
+ '(nxml-child-indent 4)
+ '(org-agenda-files (quote ("~/Documents/org/agenda/")))
+ '(org-enforce-todo-checkbox-dependencies t)
+ '(org-enforce-todo-dependencies t)
+ '(package-selected-packages
+   (quote
+    (flycheck pcre2el org-plus-contrib magit exec-path-from-shell)))
+ '(sgml-basic-offset 4)
+ '(tab-width 4))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(provide 'custom-file)
+;;; custom-file.el ends here
