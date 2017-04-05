6fd972643d812412a8846cbd0adaf121b82627f8
Aaron Madlon-Kay <aaron@madlon-kay.com>
2017-04-05 10:43:25 +0900

    Use HTTPS for Melpa

--- init.el
+++ init.el
@@ -121,7 +121,7 @@
 ;; Add melpa package repo
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("org" . "http://orgmode.org/elpa/") t)
 
