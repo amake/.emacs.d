7543705445dcb77af7153de19a27b2b6428ba1f6
Aaron Madlon-Kay <aaron@madlon-kay.com>
2017-04-06 11:23:22 +0900

    Add ivy, swiper, counsel

--- init.el
+++ init.el
@@ -141,5 +141,30 @@
           (lambda ()
             (local-set-key (kbd "C-c C-.") 'org-time-stamp-inactive))))
 
+(use-package ivy
+  :ensure counsel
+  :diminish ivy-mode
+  :bind (("C-s" . swiper)
+         ("C-c C-r" . ivy-resume)
+         ("<f6>" . ivy-resume)
+         ("M-x" . counsel-M-x)
+         ("C-x C-f" . counsel-find-file)
+         ("<f1> f" . counsel-describe-function)
+         ("<f1> v" . counsel-describe-variable)
+         ("<f1> l" . counsel-find-library)
+         ("<f2> i" . counsel-info-lookup-symbol)
+         ("<f2> u" . counsel-unicode-char)
+         ("C-c g" . counsel-git)
+         ("C-c j" . counsel-git-grep)
+         ("C-c k" . counsel-ag)
+         ("C-x l" . counsel-locate)
+         ("C-S-o" . counsel-rhythmbox)
+         :map read-expression-map
+         ("C-r" . counsel-expression-history))
+  :config
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  (ivy-mode))
+
 (provide 'init)
 ;;; init.el ends here
