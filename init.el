21e983ad38b395b48083fddc830ca6208cad26a3
Aaron Madlon-Kay <aaron@madlon-kay.com>
2017-07-26 09:41:11 +0900

    Add interactive command to open cwd in Finder

--- init.el
+++ init.el
@@ -85,6 +85,11 @@
 (global-set-key (kbd "M-<up>") 'move-line-up)
 (global-set-key (kbd "M-<down>") 'move-line-down)
 
+(defun open-pwd ()
+  "Open the current working directory in the Finder."
+  (interactive)
+  (shell-command "open ."))
+
 ;; Only on GUI
 (when window-system
   ;; Restore session
