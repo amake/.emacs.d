dffd9c01c179d1d2afb9a5bc9e52153b86c45b8a
Aaron Madlon-Kay <aaron@madlon-kay.com>
2017-11-10 00:05:43 +0900

    Move custom editing commands to separate package

--- init.el
+++ init.el
@@ -55,66 +55,6 @@
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-;; Eclipse-like line moving
-;; https://www.emacswiki.org/emacs/MoveLine
-(defmacro save-column (&rest body)
-  "Note current column, do BODY, then restore column."
-  (let ((tmp (make-symbol "col")))
-    `(let ((,tmp (current-column)))
-       ,@body
-       (move-to-column ,tmp))))
-
-(defun move-lines-up ()
-  "Move the current line or region up by one line."
-  (interactive)
-  (save-current-buffer
-    (if (use-region-p)
-        (move-lines-impl t)
-      (move-line-up))))
-
-(defun move-lines-impl (&optional up)
-  "Move the current region up by one line.  Set UP to true to go up."
-  (let (deactivate-mark
-        (rstart (region-beginning))
-        (rend (region-end)))
-    (goto-char rstart)
-    (let ((start (line-beginning-position)))
-      (goto-char rend)
-      (let* ((end (if (= (current-column) 0)
-                         rend
-                         (line-beginning-position 2)))
-             (text (buffer-substring start end)))
-        (delete-region start end)
-        (forward-line (if up -1 1))
-        (push-mark)
-        (insert text)))))
-
-(defun move-line-up ()
-  "Swap the current line with the previous one."
-  (interactive)
-  (save-column
-   (transpose-lines 1)
-   (forward-line -2)))
-
-(defun move-lines-down ()
-  "Move the current line or region down by one line."
-  (interactive)
-  (save-current-buffer
-    (if (use-region-p)
-        (move-lines-impl)
-      (move-line-down))))
-
-(defun move-line-down ()
-  "Swap the current line with the next one."
-  (interactive)
-  (save-column
-   (forward-line 1)
-   (transpose-lines 1)
-   (forward-line -1)))
-
-(global-set-key (kbd "M-<up>") 'move-lines-up)
-(global-set-key (kbd "M-<down>") 'move-lines-down)
-
 (defun open-pwd ()
   "Open the current working directory in the Finder."
   (interactive)
@@ -154,6 +94,12 @@
 (require 'bind-key)
 (package-install-selected-packages)
 (setq use-package-always-ensure t)
+
+(use-package amk-edit
+  :ensure nil
+  :load-path "lisp"
+  :bind (("M-<up>" . move-lines-up)
+         ("M-<down>" . move-lines-down)))
 
 (use-package desktop
   :if (display-graphic-p)
---
+++ lisp/amk-edit.el
@@ -0,0 +1,66 @@
+;;; package --- Summary:
+;;; Custom editing commands
+
+;;; Commentary:
+
+;;; Code:
+
+;; Eclipse-like line moving
+;; https://www.emacswiki.org/emacs/MoveLine
+(defmacro save-column (&rest body)
+  "Note current column, do BODY, then restore column."
+  (let ((tmp (make-symbol "col")))
+    `(let ((,tmp (current-column)))
+       ,@body
+       (move-to-column ,tmp))))
+
+(defun move-lines-up ()
+  "Move the current line or region up by one line."
+  (interactive)
+  (save-current-buffer
+    (if (use-region-p)
+        (move-lines-impl t)
+      (move-line-up))))
+
+(defun move-lines-impl (&optional up)
+  "Move the current region up by one line.  Set UP to true to go up."
+  (let (deactivate-mark
+        (rstart (region-beginning))
+        (rend (region-end)))
+    (goto-char rstart)
+    (let ((start (line-beginning-position)))
+      (goto-char rend)
+      (let* ((end (if (= (current-column) 0)
+                         rend
+                         (line-beginning-position 2)))
+             (text (buffer-substring start end)))
+        (delete-region start end)
+        (forward-line (if up -1 1))
+        (push-mark)
+        (insert text)))))
+
+(defun move-line-up ()
+  "Swap the current line with the previous one."
+  (interactive)
+  (save-column
+   (transpose-lines 1)
+   (forward-line -2)))
+
+(defun move-lines-down ()
+  "Move the current line or region down by one line."
+  (interactive)
+  (save-current-buffer
+    (if (use-region-p)
+        (move-lines-impl)
+      (move-line-down))))
+
+(defun move-line-down ()
+  "Swap the current line with the next one."
+  (interactive)
+  (save-column
+   (forward-line 1)
+   (transpose-lines 1)
+   (forward-line -1)))
+
+(provide 'amk-edit)
+;;; amk-edit.el ends here
