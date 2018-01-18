393cade1cb8e03f6974cb2346c5c63848d7cb8cf
Aaron Madlon-Kay <aaron@madlon-kay.com>
2018-01-19 00:46:14 +0900

    Implement backlog-insert-recent-issue

--- lisp/backlog/backlog.el
+++ lisp/backlog/backlog.el
@@ -1,16 +1,66 @@
-;;; backlog.el --- Functions for working with Backlog
+;;; backlog.el --- Functions for working with Backlog -*- lexical-binding: t -*-
 
 ;;; Commentary:
+;; Requires request package (MELPA)
 
 ;;; Code:
 
 (require 'thingatpt)
+(require 'request)
+(require 'cl-lib)
 
 (defconst backlog-link-template "https://%s.backlog.jp/view/%s")
+(defconst backlog-api-template "https://%s.backlog.jp/api/v2/%s")
+
+(defcustom backlog-api-key nil
+  "Backlog API key to use for API calls."
+  :group 'backlog)
 
 (defcustom backlog-team nil
   "The Backlog team to use when opening links."
   :group 'backlog)
+
+;; Backlog REST API functions
+
+(defun backlog-api-url (&optional action)
+  "Get the Backlog API endpoint for an ACTION."
+  (format backlog-api-template backlog-team (or action "")))
+
+(defun backlog-insert-recent-issue ()
+  "Retrieve your own recently viewed issues asynchronously and prompt to insert into buffer."
+  (interactive)
+  (backlog-recent-issues-async
+   nil
+   (lambda (issues)
+     (let* ((descs (mapcar (lambda (item) (backlog-issue-short-desc (alist-get 'issue item))) issues))
+            (selection (completing-read "Issue:" descs)))
+       (insert selection)))))
+
+(defun backlog-issue-short-desc (issue)
+  "Extract a short description from a raw ISSUE, in the format `KEY SUMMARY`."
+  (let ((key (alist-get 'issueKey issue))
+        (summary (alist-get 'summary issue)))
+    (decode-coding-string (format "%s %s" key summary) 'utf-8)))
+
+(defun backlog-recent-issues-async (user callback)
+  "Retrieve USER's recently viewed issues asynchronously and \
+process with CALLBACK.  If USER is nil, `myself` is used."
+  (let* ((usr (or user "myself"))
+         (endpoint (format "users/%s/recentlyViewedIssues" usr)))
+    (request
+     (backlog-api-url endpoint)
+     :params `(("apiKey" . ,backlog-api-key))
+     :parser 'json-read
+     :success
+     (cl-function (lambda (&key data &allow-other-keys)
+                    (condition-case err
+                        (funcall callback data)
+                      (error (message "Callback error: %s" err)))))
+     :error
+     (cl-function (lambda (&key data &allow-other-keys)
+                    (error "Request error: %s" data))))))
+
+;; External browser functions
 
 (defun browse-backlog-issue (issue)
   "Open Backlog issue with key ISSUE."
