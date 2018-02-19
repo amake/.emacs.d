07760f3c97d3b79746cafdf7a66639fa4936456e
Aaron Madlon-Kay <aaron@madlon-kay.com>
2018-02-19 16:11:33 +0900

    Customize shell prompt with current Projectile project

---
+++ init_bash.sh
@@ -0,0 +1,26 @@
+{
+    function emacs_eval() {
+        emacsclient --eval "(with-current-buffer (window-buffer) $@)"
+    }
+    function emacs_project_name() {
+        emacs_eval '(projectile-reset-cached-project-name) (projectile-project-name)'
+    }
+    function emacs_project_root() {
+        emacs_eval '(projectile-reset-cached-project-root) (projectile-project-root)'
+    }
+    function gen_prompt() {
+        project=$(emacs_project_name | tr -d '"\n')
+        if [[ "$project" = '-' ]]; then
+            PS1="\h:\W$ "
+        else
+            root=$(emacs_project_root | tr -d '"\n')
+            if [[ "$PWD/" = "$root" ]]; then
+                PS1="[$project]$ "
+            else
+                PS1="[$project]\W$ "
+            fi
+        fi
+    }
+    PROMPT_COMMAND="gen_prompt"
+} &> /dev/null
+echo
