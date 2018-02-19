68e8be6c8327f6b5b5763a60881ccb23439422ef
Aaron Madlon-Kay <aaron@madlon-kay.com>
2018-02-19 23:20:19 +0900

    Reduce recalculation of project name

--- init_bash.sh
+++ init_bash.sh
@@ -3,17 +3,20 @@
         emacsclient --eval "(with-current-buffer (window-buffer) $@)"
     }
     function emacs_project_name() {
-        emacs_eval '(projectile-reset-cached-project-name) (projectile-project-name)'
+        emacs_eval '(projectile-project-name)'
     }
     function emacs_project_root() {
-        emacs_eval '(projectile-reset-cached-project-root) (projectile-project-root)'
+        emacs_eval '(projectile-project-root)'
     }
     function gen_prompt() {
+        # This resets the project name
         emacs_eval '(rename-buffer-with-project)' &> /dev/null
         project=$(emacs_project_name | tr -d '"\n')
         if [[ "$project" = '-' ]]; then
             PS1="\h:\W$ "
         else
+            # Manually reset project root
+            emacs_eval '(projectile-reset-cached-project-root)' &> /dev/null
             root=$(emacs_project_root | tr -d '"\n')
             if [[ "$PWD/" = "$root" ]]; then
                 PS1="[$project]$ "
