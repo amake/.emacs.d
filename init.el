82f17bd269a5cbb7efb1a279ce26d0b9256bda54
Aaron Madlon-Kay <aaron@madlon-kay.com>
2019-02-05 11:14:38 +0900

    Don't add to `projectile-project-root-files-bottom-up'
    I really only want VCS roots to be projects, whereas these additions make
    projectile recognize subprojects as well.

--- init.el
+++ init.el
@@ -781,21 +781,17 @@ not be synced across machines.")
   :after (company lsp-mode))
 
 (use-package lsp-java
-  :after (projectile lsp-mode)
+  :after lsp-mode
   :hook (java-mode . lsp)
-  :demand t
-  :config
-  (add-to-list 'projectile-project-root-files-bottom-up "build.gradle")
-  (add-to-list 'projectile-project-root-files-bottom-up "pom.xml"))
+  :demand t)
 
 (use-package dart-mode
   :hook (dart-mode . lsp)
-  :after (lsp projectile)
+  :after lsp
   :ensure-system-package (dart_language_server . "pub global activate dart_language_server")
   :custom
   (dart-format-on-save t)
-  (dart-sdk-path "/Applications/flutter/bin/cache/dart-sdk/")
-  (add-to-list 'projectile-project-root-files-bottom-up "pubspec.yaml"))
+  (dart-sdk-path "/Applications/flutter/bin/cache/dart-sdk/"))
 
 (use-package flutter
   :ensure nil
