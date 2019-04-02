424c1d83d6403eefc244537094b248cec9a066b3
Aaron Madlon-Kay <aaron@madlon-kay.com>
2019-04-02 21:52:05 +0900

    Properly calculate scale-to-fit scale, allow shrinking only

--- init.el
+++ init.el
@@ -776,15 +776,24 @@ not be synced across machines.")
 (use-package face-remap
   :ensure nil
   :config
-  (defun scale-text-to-fit (width)
-   "Scale text down if window is narrower than WIDTH."
-   (let ((scale (if (< (window-body-width) width) -0.5 0)))
-     (unless (= scale text-scale-mode-amount)
+  (defun scale-text-to-fit (width shrink-only)
+   "Scale text down if window is narrower than WIDTH. If
+SHRINK-ONLY is non-nil, do not enlarge text beyond scale 0."
+   (let* ((curr-width (- (window-body-width)
+                         (line-number-display-width)))
+          (raw-scale (log (/ curr-width (float width))
+                      text-scale-mode-step))
+          (scale (if (and shrink-only (> raw-scale 0))
+                               0
+                             raw-scale)))
+     ;; (message "Scaling text: curr-width: %d, target: %d, scale: %f"
+     ;;          curr-width width scale)
+     (unless (or (= scale text-scale-mode-amount))
        (text-scale-set scale))))
-  (defmacro scale-text-to-fit-setup (width)
+  (defmacro scale-text-to-fit-setup (width shrink-only)
     "Set hooks to call `scale-text-to-fit' when appropriate."
     `(let ((fun (lambda (&optional _)
-                  (scale-text-to-fit ,width))))
+                  (scale-text-to-fit ,width ,shrink-only))))
        (add-hook 'hack-local-variables-hook fun nil t)
        (add-hook 'window-size-change-functions fun nil t))))
 
@@ -800,7 +809,7 @@ not be synced across machines.")
   (put 'dart-formatter-line-length 'safe-local-variable #'integerp)
   (defun dart-scale-text-to-fit ()
     "Adjust text scale to fit for Dart files."
-    (scale-text-to-fit-setup dart-formatter-line-length)))
+    (scale-text-to-fit-setup dart-formatter-line-length t)))
 
 (use-package flutter
   :ensure nil
