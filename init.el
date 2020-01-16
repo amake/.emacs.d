3966f8e8da741985c8385c5867db1121089f8aa0
Aaron Madlon-Kay <aaron@madlon-kay.com>
2020-01-16 11:57:24 +0900

    Configure treemacs, add treemacs-projectile and lsp-treemacs

--- init.el
+++ init.el
@@ -1005,5 +1005,24 @@ not be synced across machines.")
   :config
   (set-face-background 'mmm-default-submode-face nil))
 
+(use-package treemacs
+  :hook (treemacs-mode . (lambda () (display-line-numbers-mode -1)))
+  :bind (:map global-map
+              ("M-0" . treemacs-select-window)
+              ("C-x t 1" . treemacs-delete-other-windows)
+              ("C-x t t" . treemacs)
+              ("C-x t B" . treemacs-bookmark)
+              ("C-x t C-t" . treemacs-find-file)
+              ("C-x t M-t" . treemacs-find-tag)
+              ("C-x t s" . treemacs-switch-workspace)
+              :map treemacs-mode-map
+              ("S" . treemacs-switch-workspace)))
+
+(use-package lsp-treemacs
+  :after (treemacs lsp))
+
+(use-package treemacs-projectile
+  :after (treemacs projectile))
+
 (provide 'init)
 ;;; init.el ends here
