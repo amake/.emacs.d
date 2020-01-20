ee107e1638f4dc3623a3cd865e31cba90c057012
Aaron Madlon-Kay <aaron@wovn.io>
2020-01-20 16:20:58 +0900

    Restore org-plus-contrib
    For additional org-babel languages (Ruby)

--- init.el
+++ init.el
@@ -290,6 +290,7 @@ not be synced across machines.")
     (setq browse-url-generic-program "open")))
 
 (use-package org
+  :ensure org-plus-contrib
   :custom
   (org-enforce-todo-checkbox-dependencies t)
   (org-enforce-todo-dependencies t)
