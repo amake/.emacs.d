c01875cb81703b6973aad13bf9a6d11fdf849f7f
Aaron Madlon-Kay <aaron@wovn.io>
2020-06-12 17:58:24 +0900

    Add toggle-appearance-mode

--- lisp/amk-macos.el
+++ lisp/amk-macos.el
@@ -40,5 +40,34 @@
     (shell-command (concat "touch " file))
     (shell-command (concat "date -r " file))))
 
+(defvar amk-mac-appearance-mode nil)
+
+(defun set-mac-appearance-mode (mode)
+  "Set Mac appearance MODE (light, dark, auto) independently of system settings."
+  (setq amk-mac-appearance-mode mode)
+  (cond ((eq mode 'light)
+         (setq frame-background-mode 'light)
+         (set-background-color "white")
+         (set-foreground-color "black"))
+        ((eq mode 'dark)
+         (setq frame-background-mode 'dark)
+         (set-background-color "black")
+         (set-foreground-color "white"))
+        (t
+         (setq frame-background-mode nil)
+         (set-background-color "mac:textBackgroundColor")
+         (set-foreground-color "mac:textColor")))
+  (mapc #'frame-set-background-mode (frame-list)))
+
+(defun toggle-appearance-mode ()
+  "Toggle between light mode, dark mode, and automatic."
+  (interactive)
+  (let* ((mode amk-mac-appearance-mode)
+         (next (cond ((eq mode 'light) 'dark)
+                     ((eq mode 'dark) nil)
+                     (t 'light))))
+    (set-mac-appearance-mode next)
+    (message "%s" (or next "auto"))))
+
 (provide 'amk-macos)
 ;;; amk-macos.el ends here
