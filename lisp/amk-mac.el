9ebaa371aa414fe97d87c6506044a54be4b44f92
Aaron Madlon-Kay <aaron@wovn.io>
2020-08-11 17:50:49 +0900

    Disable toggling Mac appearance when not on Mac port

--- lisp/amk-mac.el
+++ lisp/amk-mac.el
@@ -62,23 +62,28 @@
 (defun amk-mac-apply-appearance-mode (mode)
   "Set Mac appearance MODE (light, dark, auto) on FRAME.
 
-If FRAME is nil then apply to all frames in `frame-list'."
-  (cond ((eq mode 'light)
-         (setq frame-background-mode 'light)
-         (set-background-color "white")
-         (set-foreground-color "black"))
-        ((eq mode 'dark)
-         (setq frame-background-mode 'dark)
-         (set-background-color "black")
-         (set-foreground-color "white"))
-        (t
-         (setq frame-background-mode nil)
-         (set-background-color "mac:textBackgroundColor")
-         (set-foreground-color "mac:textColor")))
+If FRAME is nil then apply to all frames in `frame-list'.
+
+Only works on Yamamoto Mitsuharu's Mac port."
+  (when (eq window-system 'mac)
+    (cond ((eq mode 'light)
+           (setq frame-background-mode 'light)
+           (set-background-color "white")
+           (set-foreground-color "black"))
+          ((eq mode 'dark)
+           (setq frame-background-mode 'dark)
+           (set-background-color "black")
+           (set-foreground-color "white"))
+          (t
+           (setq frame-background-mode nil)
+           (set-background-color "mac:textBackgroundColor")
+           (set-foreground-color "mac:textColor"))))
   (mapc #'frame-set-background-mode (frame-list)))
 
 (defcustom amk-mac-appearance-mode nil
   "Mac appearance mode: 'light, 'dark, or nil (auto).
+
+Only works on Yamamoto Mitsuharu's Mac port, not the NS port.
 
 Programmatically set with `amk-mac-set-appearance-mode'."
   :group 'amk-mac
