56e7049251c129daf3b7468de68536f9f7dc8c92
Aaron Madlon-Kay <aaron@wovn.io>
2020-11-16 11:53:06 +0900

    Always do --follow for git log

--- transient/values.el
+++ transient/values.el
@@ -1,1 +1,2 @@
-((magit-diff:magit-status-mode "--no-ext-diff" "--color-moved=default"))
+((magit-diff:magit-status-mode "--no-ext-diff" "--color-moved=default")
+ (magit-log:magit-log-mode  "--follow"))
