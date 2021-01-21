c14a761f6883f7d000310de9a70f5c0c451dbc54
Aaron Madlon-Kay <aaron@madlon-kay.com>
2021-01-22 08:07:58 +0900

    Remove no-longer-needed lsp-ui face customizations

--- custom-file.el
+++ custom-file.el
@@ -12,13 +12,6 @@
  '(amk-mac-appearance-mode 'dark)
  '(package-selected-packages
    '(vterm inf-ruby diff-hl edit-indirect lsp-sourcekit ob-async org-pdftools lsp-ivy lsp-dart json-mode ruby-test-mode yaml-mode org-plus-contrib treemacs-projectile lsp-treemacs treemacs cmake-mode jq-mode beacon reformatter dart-mode yasnippet pyvenv lsp-java flycheck-kotlin kotlin-mode lsp-ui lsp-mode flycheck-package which-key forge web-mode all-the-icons-ivy helpful apache-mode hide-lines auto-sudoedit rainbow-mode company company-quickhelp vlf rainbow-delimiters request free-keys swift-mode paredit org-pdfview pdf-tools dumb-jump dockerfile-mode diminish use-package-ensure-system-package go-mode py-autopep8 typescript-mode counsel-projectile projectile dash-at-point dired-collapse expand-region graphviz-dot-mode groovy-mode counsel markdown-mode use-package flycheck pcre2el magit exec-path-from-shell)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(lsp-ui-sideline-code-action ((t :foreground "orange")))
- '(lsp-ui-sideline-current-symbol ((t :foreground "brown" :weight ultra-bold :box (:line-width -1 :color "brown") :height 0.99))))
 
 (provide 'custom-file)
 ;;; custom-file.el ends here
